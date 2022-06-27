d47a933cd075adadc013f0a2e369cd3d6a87aec7
JayanaIllukwatte <jayana.i@aeturnum.com>
2022-06-27 09:19:07 +0530

    db added

---
+++ deploy.bat
@@ -0,0 +1,4 @@
+del "C:\wildfly-18.0.1.Final\standalone\deployments\oxcentra_V1.00.war"
+del "C:\wildfly-18.0.1.Final\standalone\deployments\oxcentra_V1.00.war.deployed"
+
+copy C:\Aeturnum\OXCENTRA\warranty\oxcentra-warranty\target\oxcentra_V1.00.war C:\wildfly-18.0.1.Final\standalone\deployments\oxcentra_V1.00.war
